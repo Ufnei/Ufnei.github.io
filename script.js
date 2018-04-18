e2776e4f828095015e583618efa09f9c0b46e484
Ufnei <pro32472@gmail.com>
2018-04-18 13:30:11 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+menu.onclick = function myF() {
+	var x = document.getElementById('MyTopNav')
+	if (x.className === "top_nav"){
+		x.className +=	" responsive";
+	} else {
+		x.className = "top_nav";
+	}
+}
