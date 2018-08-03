861248f48eb90d3678503842bc1db47cb6ba9543
Micuro <pro32472@gmail.com>
2018-08-03 14:13:38 +0400

    4

--- js/script.js
+++ js/script.js
@@ -89,30 +89,6 @@ document.querySelector('#menu_button_close').onclick = function() {
 
 
 
-$(document).ready(function() {  /*Отправка сообщений на почту*/
-
-  //E-mail Ajax Send
-  $("form").submit(function() { //Change
-    var th = $(this);
-    $.ajax({
-      type: "POST",
-      url: "mail.php", //Change
-      data: th.serialize()
-    }).done(function() {
-      alert("Thank you!");
-      setTimeout(function() {
-        // Done Functions
-        th.trigger("reset");
-      }, 1000);
-    });
-    return false;
-  });
-
-});
-
-
-
-
 
 
 
